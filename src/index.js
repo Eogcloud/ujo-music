8640354a9440031e7a3ee3bf814ffc9fe7666610
Sean King <sean7thomasking@gmail.com>
2018-11-18 14:44:50 +0000

    added redux tools

--- src/index.js
+++ src/index.js
@@ -9,12 +9,15 @@ import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux'
-import rootReducer from './reducers/reducers'
+import rootReducer from './reducers/reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 const store = createStore(
    rootReducer,
+   composeWithDevTools(
    applyMiddleware(thunk)
    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+));
 
 ReactDOM.render(
    <Provider store={store}>
